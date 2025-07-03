77429f519ed4b8450b9e13b46f6768712780ec0f
Skitch49 <delaunay.alexis49@gmail.com>
2025-07-03 18:08:55 +0200

    add question feature with votes and comments

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Question;
+use App\Form\CommentForm;
 use App\Form\QuestionForm;
-use App\Form\QuestionType;
+use App\Repository\QuestionRepository;
+use DateTime;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,15 +19,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class QuestionController extends AbstractController
 {
     #[Route('/ask', name: 'form')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-
-        $formQuestion = $this->createForm(QuestionForm::class);
+        $question = new Question();
+        $formQuestion = $this->createForm(QuestionForm::class, $question);
 
         $formQuestion->handleRequest($request);
 
         if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-            dd($formQuestion->getData());
+
+            $question->setNbrOfResponse(0)
+                ->setRating(0)
+                ->setCreateAt(new DateTimeImmutable());
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('success', 'Votre question à été ajoutée avec succès');
+            return $this->redirectToRoute("home");
         }
 
         return $this->render('question/index.html.twig', [
@@ -30,22 +43,45 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/{id}', name: 'show')]
-    public function show(Request $request, string $id): Response
+    public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentForm::class, $comment);
 
-        $question =   [
-            'title' => 'Je suis une super question',
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-            'rating' => 20,
-            'author' => [
-                'name' => 'Jean Dupont',
-                'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
-            ],
-            'nbrOfResponse' => 15
-        ];
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setCreateAt(new DateTimeImmutable())
+                ->setQuestion($question)
+                ->setRating(0);
+            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
+            $em->persist($comment);
+            $em->flush();
+            $this->addFlash('success', 'Commentaire ajouté avec succès');
+            return $this->redirect($request->getUri());
+        }
 
         return $this->render('question/show.html.twig', [
             'question' => $question,
+            'commentForm' => $commentForm->createView()
         ]);
+    }
+
+    #[Route('/rating/{id}/{score}', name:'rating')]
+    public function ratingQuestion(Request $request,Question $question,int $score,EntityManagerInterface $em){
+        $question->setRating($question->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
+
+    }
+
+        #[Route('/comment/rating/{id}/{score}', name:'comment_rating')]
+    public function ratingComment(Request $request,Comment $comment,int $score,EntityManagerInterface $em){
+        $comment->setRating($comment->getRating() + $score);
+        $em->flush();
+        $referer = $request->server->get('HTTP_REFERER');
+        return $referer ? $this->redirect($referer) : $this->redirectToRoute('home');
+
     }
 }
---
+++ src/Entity/Question.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\QuestionRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank(message: "Votre titre est vide")]
+    #[Assert\Length(min:20,minMessage:"Veuillez détailler votre titre",max:255,maxMessage:"Le titre de votre question est trop long")]
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[Assert\NotBlank(message: "Votre contenu est vide")]
+    #[Assert\Length(min:100,minMessage:"Veuillez détailler votre question")]
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createAt = null;
+
+    #[ORM\Column]
+    private ?int $rating = null;
+
+    #[Assert\PositiveOrZero(message:"Le nombre de réponses est négative")]
+    #[ORM\Column]
+    private ?int $nbrOfResponse = null;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'question')]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $createAt): static
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getNbrOfResponse(): ?int
+    {
+        return $this->nbrOfResponse;
+    }
+
+    public function setNbrOfResponse(int $nbrOfResponse): static
+    {
+        $this->nbrOfResponse = $nbrOfResponse;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getQuestion() === $this) {
+                $comment->setQuestion(null);
+            }
+        }
+
+        return $this;
+    }
+}
